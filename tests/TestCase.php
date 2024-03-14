11cfe7daa4eaa3fd94f7017438a898f5b7e2eaed
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2024-03-14 16:15:46 +0700

    WIP
    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,23 +4,19 @@ namespace Wkd\Tests;
 
 use DI\Bridge\Slim\Bridge;
 use PHPUnit\Framework\TestCase as PHPUnitTestCase;
-use Prophecy\PhpUnit\ProphecyTrait;
 use Slim\App;
 use Wkd\Application\RouteDefinitions;
 use Wkd\Application\SlimRunner;
 
 class TestCase extends PHPUnitTestCase
 {
-    use ProphecyTrait;
 
     protected $runner;
-    protected $faker;
 
     protected function setUp(): void
     {
         parent::setUp();
         $this->runner = new SlimRunner(dirname(__DIR__));
-        $this->faker  = \Faker\Factory::create();
     }
 
     /**
