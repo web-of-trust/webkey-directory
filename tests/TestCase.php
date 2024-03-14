00474f5993eae939bbe876f52bf084c1622b2f85
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2024-03-14 08:57:48 +0700

    WIP
    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

---
+++ tests/TestCase.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types=1);
+
+namespace Wkd\Tests;
+
+use DI\Bridge\Slim\Bridge;
+use PHPUnit\Framework\TestCase as PHPUnitTestCase;
+use Prophecy\PhpUnit\ProphecyTrait;
+use Slim\App;
+use Wkd\Application\SlimRunner;
+
+class TestCase extends PHPUnitTestCase
+{
+    use ProphecyTrait;
+
+    protected $runner;
+    protected $faker;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->runner = new SlimRunner(dirname(__DIR__));
+        $this->faker  = \Faker\Factory::create();
+    }
+
+    /**
+     * @return App
+     */
+    protected function getAppInstance(): App
+    {
+        $app = Bridge::create($this->runner->getContainer());
+        $app->addRoutingMiddleware();
+        return $app;
+    }
+}
