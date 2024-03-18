078e7f6946c23c64a37afdc154ae42ccf7d79198
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2024-03-18 21:34:36 +0700

    WIP
    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

--- tests/CommandsTest.php
+++ tests/CommandsTest.php
@@ -8,7 +8,7 @@ use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Tester\CommandTester;
 use Wkd\Command\SyncKeyCommand;
 
-class SynKeyCommandTest extends TestCase
+class CommandsTest extends TestCase
 {
     private $console;
 
